e81e51cc6cc739d570f3cc0969b233bb0136572f
BayuDC <dev.bayudc@gmail.com>
2022-03-14 14:38:24 +0700

    Add start button

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: {},
+    methods: {
+        start() {
+            this.playing = true;
+        },
+    },
+    data() {
+        return {
+            playing: false,
+        };
+    },
+};
 </script>
+
+<template>
+    <h1>The Reaction Game</h1>
+    <button @click="start">Start</button>
+</template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: rgb(52, 73, 94);
+    margin-top: 60px;
+}
+#app button {
+    font-size: 16px;
+    border: none;
+    background: rgb(65, 184, 131);
+    color: white;
+    padding: 10px 20px;
 }
 </style>
